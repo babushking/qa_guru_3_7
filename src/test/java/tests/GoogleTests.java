54e038b6160489faa3c78447354a9b8c6f5ec094
QAguru3Student <75329574+QAguru3Student@users.noreply.github.com>
2020-12-01 20:55:24 +0300

    added selenideSearchTest

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -1,3 +1,14 @@
-public class GoogleTests{
+public class GoogleTests {
 
+    @Test
+    void selenideSearchTest() {
+        // Открыть google
+        open("https://google.com");
+
+        // Ввести Selenide в поиск
+        $(byName("q")).setValue("Selenide").pressEnter();
+
+        // Проверить, что Selenide появился в результатах поиска
+        $("html").shouldHave(Condition.text("selenide.org"));
+    }
 }
