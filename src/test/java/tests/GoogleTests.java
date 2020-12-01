8ce0079b92fd8ab5070e3b5b0bcb9aceaa72304b
Horoshiy <admin@2liga.ru>
2020-12-01 22:04:30 +0300

    add imports and selenide dependency

--- src/test/java/tests/GoogleTests.java
+++ src/test/java/tests/GoogleTests.java
@@ -1,3 +1,12 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.byName;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class GoogleTests {
 
     @Test
